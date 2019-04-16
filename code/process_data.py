01b106165568cc9307ae393b37ad966cf9b927ec
likgoek <jonathanstroem91@gmail.com>
2019-04-16 10:54:49 -0400

    New (larger) dataframe containing both data for 420 and 422

--- code/process_data.py
+++ code/process_data.py
@@ -5,8 +5,8 @@ import seaborn as sns
 import os
 import matplotlib.pyplot as plt
 
+# method that finds the dropbox install directory
 dropbox_folder = None
-
 def get_dropbox_path():
     for dirname, dirnames, filenames in os.walk(os.path.expanduser('~')):
         for subdirname in dirnames:
@@ -17,51 +17,61 @@ def get_dropbox_path():
             break
     return dropbox_folder
 
-# TODO: create this function to intelligently find infs
-def replace_inf(df):
-    df.replace([-np.inf,np.inf], np.nan)
-    return
-
 def get_season(x):
-    seasons = {
-        'Winter': (12, 2),
-        'Spring': (3, 5),
-        'Summer': (6, 8),
-        'Fall': (9, 11),
-    }
-    if (x == 12) or (x == 1) or (x == 2):
+    if x in (12, 1, 2):
         return 'Winter'
-    elif (x == 3) or (x == 4) or (x == 5):
+    elif x in (3, 4, 5):
         return 'Spring'
-    elif (x == 6) or (x == 7) or (x == 8):
+    elif x in (6, 7, 8):
         return 'Summer'
-    elif (x == 9) or (x == 10) or (x == 11):
+    elif x in (9, 10, 11):
         return 'Fall'
     else:
         return 'Error'
 
+"""
+Generates huge dataframes using the Indianapolis database.
+All factors of interest are processed individually, forming many "smaller" dataframes.
+These are then stiched together using the merge_asof method.
+The basis for these mergers is the indoor air contaminant concentrations dataframe (since which factors determines this is what we're trying to understand).
+Before these mergers occur, the times that fall outside the period the SSD was yet to be installed are discarded.
+Some additional computational steps, e.g. calculating attenuation factors are also performed.
+This process is repeated for both sides of the house (420 and 422).
+
+Factors considered:
+* Indoor air contaminant concentration
+* Indoor/outdoor and indoor/subslab pressure difference
+* Snow depth, rainfall, windspeed
+* Subslab contaminant concentration
+* Outdoor, indoor, and soil temperature.
+"""
 class Indianapolis:
-    def __init__(self, side='422'):
-        self.side = side
+    def __init__(self):
         self.db = sqlite3.connect(get_dropbox_path() + '/var/Indianapolis.db')
 
+        dfs = [] # list to store dataframes
+        for self.side in ('420', '422'):
+            df = self.get_data() # gets dataframe for one side
+            df.to_csv('./data/indianapolis_%s.csv' % self.side) # saves that dataframe
+            df['Side'] = np.repeat(self.side, len(df)) # adds new column telling us which side the data belongs to
+            dfs.append(df) # appends the dataframe
 
-        df = self.get_data()
-        df.to_csv('./data/indianapolis_%s.csv' % side)
-
+        pd.concat(dfs, sort=False).to_csv('./data/indianapolis.csv') # concats the two sides dataframes and saves the data
         return
 
     def get_data(self):
         # loads individual dataframes
-
         indoor = self.get_indoor_air() # this is the basis for all df merging (i.e. we wish to fit all time stamps to these)
-        ssd = self.get_ssd_status()
-        subslab = self.get_subslab() # special because we need to match species
+        ssd = self.get_ssd_status() # this df contains the time period when the mitigations system was yet to be installed
+        subslab = self.get_subslab() # subslab contaminant concentration, which is special because we need to match species
 
+        # merges the indoor and subslab contaminant concentrations by contaminant type
         df = pd.merge_asof(indoor, subslab, left_index=True, right_index=True, by='Contaminant')
+
+        # removes time that do not fall under the SSD-system "not yet installed" period
         df = df.loc[(df.index.date>=ssd.index.date.min()) & (df.index.date<=ssd.index.date.max())]
 
-        # tuples of dataframes/data to fit to our indoor concentration data
+        # tuple of dataframes/data to fit to our indoor concentration data
         dfs_to_merge = (
             self.get_meteorological_data(),
             self.get_pressure(),
@@ -72,12 +82,12 @@ class Indianapolis:
         for _ in dfs_to_merge:
             df = pd.merge_asof(df, _, left_index=True, right_index=True,)
 
-
         # new columns/calculations
         df['AttenuationSubslab'] = df['IndoorConcentration']/df['SubslabConcentration']
         df['AttenuationSubslab'] = df['AttenuationSubslab'].replace([-np.inf,np.inf], np.nan)
         df['logIndoorConcentration'] = df['IndoorConcentration'].apply(np.log10).replace([-np.inf,np.inf], np.nan)
         df['logAttenuationSubslab'] = df['AttenuationSubslab'].apply(np.log10).replace([-np.inf,np.inf], np.nan)
+        # returns the season for each timestamp
         df['Season'] = df.index.month.map(get_season)
 
         return df
@@ -243,7 +253,6 @@ class Indianapolis:
         df = df.pivot(columns='Variable', values='Value')
 
         return df
-
     def get_soil_temp(self):
         query = "\
             SELECT \
@@ -269,5 +278,5 @@ class Indianapolis:
         df.rename(columns=rename, inplace=True)
 
         return df
-Indianapolis(side='422')
-Indianapolis(side='420')
+
+Indianapolis()
