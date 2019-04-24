f2ccf872f224ea4e03fa478645f5b6a3974de0f8
likgoek <jonathanstroem91@gmail.com>
2019-04-24 14:38:24 -0400

    Working Kriging example

--- code/kriging.py
+++ code/kriging.py
@@ -1,10 +1,29 @@
-from sklearn.datasets import make_friedman2
+import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import DotProduct, WhiteKernel
+
+# observation coordinates
+X = np.array([
+    [1, 2],
+    [1.5, 4],
+    [0.5, 1.5],
+    [5,8],
+    [4, 3],
+    [2.5, 6.5],
+])
+y = np.array([0.5, 1.5, 4.5, 3, 2, 6]) # observation data
+
+gpr = GaussianProcessRegressor() # regressor function
+gpr.fit(X, y)
+
+res = 100 # prediction resolution
+x1, x2 = np.meshgrid(np.linspace(0, 7, res), np.linspace(0, 10, res)) # grid to predict values onto
+xx = np.vstack([x1.reshape(x1.size), x2.reshape(x2.size)]).T # stacks gridpoints
+y_pred = gpr.predict(xx) # predicts values onto the grid
+y_pred = y_pred.reshape((res, res)) # reshapes predicted values
 
 
-X, y = make_friedman2(n_samples=500, noise=0, random_state=0)
-kernel = DotProduct() + WhiteKernel()
-gpr = GaussianProcessRegressor(kernel=kernel, random_state=0).fit(X, y)
-gpr.score(X, y)
-gpr.predict(X[:2,:], return_std=True)
+# plotting
+plt.contourf(x1,x2, y_pred)
+plt.scatter(X[:,0], X[:,1], c=y)
+plt.show()
