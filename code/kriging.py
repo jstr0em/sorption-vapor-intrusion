71553620ab95eba1afca62af2b635136c0c681eb
likgoek <jonathanstroem91@gmail.com>
2019-05-06 16:23:13 -0400

    Animation

--- code/kriging.py
+++ code/kriging.py
@@ -2,11 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.gaussian_process import GaussianProcessRegressor
 import pandas as pd
-import itertools
 import sqlite3
 from math import isnan
 from sklearn.gaussian_process.kernels import ConstantKernel, RBF, Matern
 from utils import get_dropbox_path
+from plotly.offline import download_plotlyjs, init_notebook_mode,  iplot, plot
+#init_notebook_mode(connected=True)
 
 class Data:
     # static variable
@@ -71,14 +72,17 @@ class Kriging(Data):
     def __init__(self):
         data_processing = Data()
 
-        data = data_processing.get_data() # TODO: Dynamically detect NaNs and update probe locations based on that...
+        data = data_processing.get_data(interpolate=True) # TODO: Dynamically detect NaNs and update probe locations based on that...
 
 
         coords, vals = self.get_coords_vals(data[data['Date']=='2011-01-07'])
         x1, x2, x3, grid = self.get_meshgrid()
         pred = self.kriging(coords, vals, grid)
-        print(pred)
         predictions, grids = {}, {}
+
+
+        self.x1, self.x2, self.x3, self.grid = x1, x2, x3, grid
+        self.pred = pred
         """
         for time in data['Date'].unique():
         predictions = {}
@@ -94,7 +98,12 @@ class Kriging(Data):
         #self.plot_results(x1, x2, predictions)
         return
 
-
+    def get_results(self,log=False):
+        if log is False:
+            return self.x1, self.x2, self.x3, self.grid, self.pred
+        elif log is True:
+            self.pred = np.log10(self.pred)
+            return self.x1, self.x2, self.x3, self.grid, self.pred
     def load_probes_coordinates(self):
         probe_coords = pd.read_csv('./data/indianapolis_probes.csv')
         return probe_coords
@@ -128,12 +137,12 @@ class Kriging(Data):
         # TODO: Lookup if specific values are use for geophysics or which kernel is best (might need to make a custom one)
 
         kernel = ConstantKernel()*RBF()
+        #kernel = RBF()
         gpr = GaussianProcessRegressor(kernel=kernel) # regressor function
         gpr.fit(coords, vals)
 
         pred = gpr.predict(grid) # predicts values onto the grid
         pred = pred.reshape((self.res, self.res, 50)) # reshapes predicted values
-
         return pred
 
     def get_meshgrid(self):
@@ -143,44 +152,116 @@ class Kriging(Data):
         grid = np.vstack([x1.reshape(x1.size), x2.reshape(x2.size), x3.reshape(x3.size)]).T # stacks gridpoints
         return x1, x2, x3, grid
 
-    def plot_results(self, x1, x2, predictions):
-        from matplotlib import animation
-        times, plot_data = [], []
-
-        for key, val in predictions.items():
-            times.append(key)
-            plot_data.append(val)
-
-        vmin, vmax = 0, 30
-
-        print(vmax)
-        kw = dict(levels=np.linspace(vmin, vmax, 10), cmap='jet', vmin=vmin, vmax=vmax, origin='lower')
-
-        fig, ax = plt.subplots()
-        #ax = plt.axes(xlim=(0, x1.max()), ylim=(0, x2.max()))
-        cont = ax.contourf(x1, x2, plot_data[0], **kw)
-
-        cbar = plt.colorbar(cont)
-        ax.set_xlabel('x')
-        ax.set_ylabel('y')
-        # animation function
-        def animate(i):
-            ax.clear()
-            cont = ax.contourf(x1, x2, plot_data[i], **kw)
-            ax.set_title(times[i])
-
-            return cont
-
-
-        FFwriter = animation.FFMpegWriter()
-        anim = animation.FuncAnimation(fig, animate, interval=500)
-        print(FFwriter.bin_path())
-        anim.save('animation.mp4', writer=FFwriter)
-
-        #plt.contourf(x1, x2, y_pred)
-        #plt.show()
-
-
         return
 
-Kriging()
+
+def get_lims_colors(surfacecolor):# color limits for a slice
+    return np.min(surfacecolor), np.max(surfacecolor)
+
+
+class Slices:
+    def __init__(self):
+
+        krig = Kriging()
+        x1, x2, x3, grid, pred = krig.get_results()
+
+        vmin, vmax = np.min(pred), np.max(pred)
+
+        base_trace=dict(type='surface',
+                x=x1.T[0],
+                y=x2.T[0],
+                z=x3.T[0],
+                surfacecolor=pred.T[0],
+                colorscale='jet',
+                colorbar=dict(thickness=20, ticklen=4)
+                )
+
+        frames = []
+        frame_zval = np.linspace(0,6, 50)
+        for k in range(frame_zval.shape[0]):
+            surfcol=pred.T[k]
+            cmin, cmax=get_lims_colors(surfcol)
+            vmin=min([cmin, vmin])
+            vmax=max([cmax, vmax])
+            frames.append(dict(data=[dict(z=x3.T[k],
+                                     surfacecolor=pred.T[k])],
+                               name='frame{}'.format(k)))
+
+        base_trace.update(cmin=vmin, cmax=vmax)
+
+
+
+        sliders=[dict(steps= [dict(method= 'animate',#Sets the Plotly method to be called when the
+                                                        #slider value is changed.
+                                   args= [[ 'frame{}'.format(k) ],#Sets the arguments values to be passed to
+                                                                      #the Plotly method set in method on slide
+                                          dict(mode= 'immediate',
+                                          frame= dict( duration=50, redraw= False ),
+                                                   transition=dict( duration= 0)
+                                                  )
+                                            ],
+                                    label='{:.2f}'.format(frame_zval[k])
+                                     ) for k in range(frame_zval.shape[0])],
+                        transition= dict(duration= 0 ),
+                        x=0,#slider starting position
+                        y=0,
+                        currentvalue=dict(font=dict(size=12),
+                                          prefix='z: ',
+                                          visible=True,
+                                          xanchor= 'center'
+                                         ),
+                        len=1.0)#slider length)
+                   ]
+
+
+        axis = dict(showbackground=True,
+                    backgroundcolor="rgb(230, 230,230)",
+                    gridcolor="rgb(255, 255, 255)",
+                    zerolinecolor="rgb(255, 255, 255)",
+                    )
+
+
+        layout = dict(
+                 title='Slices in volumetric data',
+            font=dict(family='Balto'),
+                 width=600,
+                 height=600,
+                 scene=dict(xaxis=(axis),
+                            yaxis=(axis),
+                            zaxis=dict(axis, **dict(range=[np.min(frame_zval), np.max(frame_zval)], autorange=False)),
+                            aspectratio=dict(x=1,
+                                             y=1,
+                                             z=1
+                                             ),
+                            ),
+                 updatemenus=[dict(type='buttons', showactive=False,
+                                        y=1,
+                                        x=1.3,
+                                        xanchor='right',
+                                        yanchor='top',
+                                        pad=dict(t=0, r=10),
+                                        buttons=[dict(label='Play',
+                                                      method='animate',
+                                                      args=[None,
+                                                            dict(frame=dict(duration=30,
+                                                                            redraw=False),
+                                                                 transition=dict(duration=0),
+                                                                 fromcurrent=True,
+                                                                 mode='immediate'
+                                                                )
+                                                           ]
+                                                     )
+                                                ]
+                                       )
+                                  ],
+            sliders=sliders
+                )
+
+
+        fig=dict(data=[base_trace], layout=layout, frames=frames)
+
+        plot(fig, validate=False, filename='soil_slice.html')
+        return
+
+
+Slices()
