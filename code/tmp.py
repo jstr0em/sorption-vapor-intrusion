e3327078d6d3e963e92c00fd98f7051604877d6a
likgoek <jonathanstroem91@gmail.com>
2019-05-20 17:20:08 -0400

    Working on alternative representation

--- code/tmp.py
+++ code/tmp.py
@@ -1,6 +1,8 @@
 from plotly.offline import download_plotlyjs, init_notebook_mode,  iplot, plot
 import numpy as np
 import pandas as pd
+import plotly.graph_objs as go
+
 
 def get_lims_colors(surfacecolor):# color limits for a slice
     return np.min(surfacecolor), np.max(surfacecolor)
@@ -11,29 +13,58 @@ z = np.linspace(0,5,3)
 
 X, Y, Z = np.meshgrid(x,y,z)
 C = X**2+Z**2
-vmin, vmax = np.min(C), np.max(C)
+
+times = ['2011/03/12','2012/03/12']
+
+"""
+iterables = [times, z]
+
+index = pd.MultiIndex.from_product(iterables, names=['times', 'depth'])
+# stores data in df
+data_to_add = []
+for time in times:
+    for i in range(len(z)):
+        data_to_add.append(C.T[i])
+
+df = pd.Series(data_to_add, index=index)
+
+# df is in the correct format now...
+#print(df[(times[0], z[2])])
+data = []
+for time in times:
+    for depth in z:
+        data_now = dict(
+            type='surface',
+            surfacecolor=df[time, depth],
+            colorscale='jet',
+        )
+
+        data.append(data_now)
 
 
-data = [
-    dict(type='surface',
+steps = []
+
+for i in range(len(data)):
+    step = dict(
+        method='restyle',
+        args=['visible', [False] * len(data_slider)],
+        label='Year {}'.format(i + 1960)
+    )
+    step['args'][1][i] = True
+    steps.append(step)
+"""
+# alternative route
+data = []
+for i in range(3):
+    data_now = dict(
+        type='surface',
+        visible=True,
+        surfacecolor=C.T[i],
+        z=Z.T[i],
         x=X.T[0],
         y=Y.T[0],
-        z=Z.T[0],
-        surfacecolor=C.T[0],
-        colorscale='jet',
-        colorbar=dict(thickness=20, ticklen=4),
-        )
-    ]
+    )
+    data.append(data_now)
 
 
-
-
-frames = []
-for k in range(len(z)):
-    surfcol=C.T[k]
-    cmin, cmax=get_lims_colors(surfcol)
-    vmin=min([cmin, vmin])
-    vmax=max([cmax, vmax])
-    frames.append(dict(data=[dict(z=Z.T[k],
-                             surfacecolor=C.T[k])],
-                       name='frame{}'.format(k)))
+plot(data,filename='tmp.html')
