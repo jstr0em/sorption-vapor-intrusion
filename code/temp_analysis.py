d0a263848ba2cebe4ae4458fe732b2a0bdcf5085
likgoek <jonathanstroem91@gmail.com>
2019-06-10 09:58:09 -0400

    Changing formatter

--- code/temp_analysis.py
+++ code/temp_analysis.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+import matplotlib.ticker as ticker
 
 df = pd.read_csv('./data/temp_sim.csv')
 
@@ -19,9 +19,6 @@ sns.lineplot(
     style='Sub-base',
     ax=ax,
 )
-
-ax.set(
-    yscale='log',
-)
+ax.yaxis.set_major_formatter(ticker.FormatStrFormatter('%2.1e'))
 
 plt.show()
