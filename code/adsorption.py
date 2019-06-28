64f1a3cee07f2ebcec5acfa71a9c007bc15e1f9e
likgoek <jonathanstroem91@gmail.com>
2019-06-28 09:42:35 -0400

    Sorption analysis

---
+++ code/adsorption.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+
+df = pd.read_csv('../data/simulation_adsorption_p.csv', header=4, index_col=0)
+
+
+print(df)
+
+M = 131.39 # g/mol
+df['c_ads'] *= M # converts to g adsorbate/kg soil
+
+fig, axes = plt.subplots(2,2,dpi=300,sharex=True)
+
+
+titles = (
+    'Indoor concentration (mol/m^3)',
+    'Adsorbed concentration (g/kg)',
+    'Adsorbed mass (g)',
+    'Attenuation from source',
+)
+
+for col, title, ax in zip(list(df), titles, axes.flatten()):
+    df.plot(
+        ax=ax,
+        y=col,
+        title=title,
+        legend=False,
+    )
+
+
+
+axes.flatten()[0].set(
+    yscale='log',
+)
+axes.flatten()[-1].set(
+    yscale='log',
+)
+
+
+plt.tight_layout()
+plt.show()
