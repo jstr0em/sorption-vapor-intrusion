55c20fb380eddabbf7c97eafdc44309f796daf16
likgoek <jonathanstroem91@gmail.com>
2019-07-02 20:46:17 -0400

    Doing something

--- code/adsorption.py
+++ code/adsorption.py
@@ -7,19 +7,19 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('../data/simulation_adsorption_p.csv', header=4, index_col=0)
 
 
-print(df)
-
 M = 131.39 # g/mol
-df['c_ads (mol/kg)'] *= M # converts to g adsorbate/kg soil
-
-fig, axes = plt.subplots(2,2,dpi=300,sharex=True)
+df['c_ads (mol/kg)'] *= M*1e3 # converts to mg adsorbate/kg soil
+df['c_indoor (mol/m^3)'] *= M*1e6 # ug/m3
+fig, axes = plt.subplots(2,3,dpi=300,sharex=True)
 
 
 titles = (
-    'Indoor concentration (mol/m^3)',
-    'Adsorbed concentration (g/kg)',
+    'Indoor concentration (ug/m^3)',
+    'Adsorbed concentration (mg/kg)',
     'Adsorbed mass (g)',
     'Attenuation from source',
+    'Flux from below (mol/(m^2*s))',
+    'Flux from sides (mol/(m^2*s))',
 )
 
 for col, title, ax in zip(list(df), titles, axes.flatten()):
@@ -31,11 +31,24 @@ for col, title, ax in zip(list(df), titles, axes.flatten()):
     )
     ax.set(
         yscale='log',
+        xlabel='$\\Delta P_\\mathrm{in/out}$ (Pa)'
     )
 
 axes = axes.flatten()
 
+axes[1].set(
+    yscale='linear'
+)
+axes[2].set(
+    yscale='linear'
+)
 
+axes[-2].set(
+    yscale='linear'
+)
+axes[-1].set(
+    yscale='linear'
+)
 
-plt.tight_layout()
+#plt.tight_layout()
 plt.show()
