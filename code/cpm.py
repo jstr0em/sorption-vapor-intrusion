935d514daa4e9ccdfdfa8ce684fb482e30b59a5f
likgoek <jonathanstroem91@gmail.com>
2019-07-26 15:49:48 -0400

    Preparing analysis (waiting for results)

--- code/cpm.py
+++ code/cpm.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
+import matplotlib.gridspec as gridspec
 
 
 df = pd.read_csv('../data/transient_results.csv', header=4)
@@ -25,6 +25,8 @@ new_names = (
     'j_c_ck',
 )
 
+soil_names = {'1': , '2': , '3': ,}
+
 rename = dict(zip(list(df), new_names))
 
 df.rename(columns=rename, inplace=True)
@@ -32,9 +34,24 @@ df.rename(columns=rename, inplace=True)
 df = pd.concat([df, p['p_cpm']], axis=1)
 df['d_alpha'] = df['c_in']/df['c_in'][0]
 df['c_ads'] *= 131.38
-fig, (ax1, ax2) = plt.subplots(1,2,dpi=300)
 
-df.plot(x='t',y=['p_cpm','m_ads'],ax=ax1, label=['p_in (Pa)','m_ads (g)'])
-df.plot(x='t',y=['alpha','d_alpha','c_ads'],ax=ax2, logy=True, label=['alpha','d_alpha', 'c_ads (g/kg)'])
+#
+fig = plt.figure(dpi=300)
+
+gs1 = gridspec.GridSpec(3, 2, figure=fig)
+ax1 = plt.subplot(gs1[0,:])
+ax2 = ax1.twinx()
+
+ax3 = plt.subplot(gs1[1,0])
+ax4 = plt.subplot(gs1[1,1])
+
+ax5 = plt.subplot(gs1[2,0])
+ax6 = plt.subplot(gs1[2,1])
+
+
+cycle = plt.rcParams['axes.prop_cycle'].by_key()['color']
+
+df.plot(x='t',y='p_cpm',ax=ax1,legend=False)
+df.plot(x='t',y='d_alpha',ax=ax2,logy=True,legend=False,color=cycle[1])
 
 plt.show()
