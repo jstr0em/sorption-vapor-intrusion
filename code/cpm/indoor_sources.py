1da450f5df9626cc50b1208a0bbf7e83cbf2c0b0
likgoek <jonathanstroem91@gmail.com>
2019-07-31 16:05:49 -0400

    Indoor source analysis with picked values

--- code/cpm/indoor_sources.py
+++ code/cpm/indoor_sources.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from scipy.integrate import odeint
+from scipy.interpolate import interp1d
 
 transient = pd.read_csv('../../data/transient_results.csv', header=4)
 ss = pd.read_csv('../../data/steady_state_material_sweep.csv', header=4)
@@ -24,36 +25,49 @@ ss.rename(columns=dict(zip(list(ss), new_names2)), inplace=True)
 transient = pd.concat([transient, p['p_cpm']], axis=1)
 transient['c_ads'] *= 131.38
 
-K = 10
-k2 = 1e-2
-k1 = K*k2
 
 def r(c_in, c_star, K, k2):
+    k1 = K*k2
     r = k1*c_star-k2*c_in
     return r
 
 def cstr(u, t, Ae, V, K, k2):
     c_in = u[0]
     c_star = u[1]
-
-    dc_in_dt = n(t)/V - Ae*c_in + r(c_in, c_star, K, k2)
+    dc_in_dt = n(t)/V - Ae*c_in + r(c_in, c_star, K, k2)/V
     dc_star_dt = -r(c_in, c_star, K, k2)/V
 
     return [dc_in_dt, dc_star_dt]
-import scipy.integrate
 
-tmax = 10.0
+# parameters
+Ae = 0.5
+V = 300
+K = 1e-2
+k2 = 1e-4
 
-def a(t):
-    if t < tmax / 2.0:
-        return ((tmax / 2.0) - t) / (tmax / 2.0)
-    else:
-        return 1.0
+A_ck = 4*10*1e-2
 
-def func(x, t, a):
-    return - (x - a(t))
+n = interp1d(transient['t'].values, transient['j_ck'].values*A_ck*3600*1e2, fill_value=0, bounds_error=False)
+c0_in = n(0)/V/Ae
+transient['c_in'] /= transient['c_in'][0]/c0_in
 
-x0 = 0.8
-t = np.linspace(0.0, tmax, 1000)
-args = (a,)
-y = odeint(func, x0, t, args)
+t = np.linspace(0, 72, 100)
+
+
+for K in [1e-2, 1, 1e2]:
+
+    fig, (ax1, ax2) = plt.subplots(2,1,dpi=300)
+    for k2 in (1e-4, 1e-2, 1, 1e2, 1e4):
+        k1 = K*k2
+        c0 = [c0_in, c0_in/K]
+        c = odeint(func=cstr, y0=c0, t=t, args=(Ae, V, K, k2,))
+        c_in = c[:,0]
+        c_star = c[:,1]
+
+        ax1.semilogy(t,c_in,label='k1 = %1.0e, k2 = %1.0e' % (k1, k2))
+        ax2.semilogy(t,c_star,label='k2 = %1.0e' % k2)
+
+    transient.plot(x='t', y='c_in', ax=ax1, logy=True, legend=False, color='k', label='reference')
+    ax1.set_title('K = %1.0e' % K)
+    ax1.legend()
+plt.show()
