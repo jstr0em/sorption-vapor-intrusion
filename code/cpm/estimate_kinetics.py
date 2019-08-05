5f5d672f663b56e64590a9710357074f316ba8bc
likgoek <jonathanstroem91@gmail.com>
2019-08-05 17:26:35 -0400

    Generated kinetics and implications figure

---
+++ code/cpm/estimate_kinetics.py
@@ -0,0 +1,99 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from scipy.integrate import odeint
+from scipy.optimize import curve_fit, leastsq
+
+
+class Kinetics:
+
+    def __init__(self, material='concrete', contaminant='TCE', T=298, P=101325):
+        self.material = material
+        self.contaminant = contaminant
+        self.T = T
+        self.P = P
+        self.path = '../../data/adsorption_kinetics.csv'
+        return
+
+    def get_thermo_states(self):
+        return self.T, self.P
+
+    def get_material(self):
+        return self.material
+
+    def get_contaminant(self):
+        return self.contaminant
+
+    def get_gas_conc(self):
+        # gas constant
+        R = 8.31446261815324  # (J/(mol*K))
+        T, P = self.get_thermo_states()
+        part_by_part = 1.12e-9  # parts of TCE per part of air V_TCE/V_total
+        c = P * part_by_part / (R * T)  # concetration of TCE (g/m^3)
+        return c
+
+    def get_adsorbed_conc(self):
+        mass_by_mass = self.get_adsorption_data()
+        rho = self.get_material_density()
+        M = self.get_molar_mass()
+        c_star = mass_by_mass * rho / 1e9 / M  # g/m3
+        return c_star
+
+    def get_molar_mass(self):
+        contaminant = self.get_contaminant()
+        # molar masses are in (g/mol)
+        M = {'TCE': 131.38}
+        return M[contaminant]
+
+    def get_data_path(self):
+        return self.path
+
+    def get_material_density(self):
+        material = self.get_material()
+        # densities in g/m^3
+        density = {'drywall': 0.6e6, 'concrete': 2.0e6,
+                   'carpet': 1.314e6, 'wood': 0.86e6, 'paper': 0.8e6, }
+        return density[material]
+
+    def rate_equation(self, c_star, t, k1, k2):
+        c = self.get_gas_conc()
+        r = k1 * c_star - k2 * c
+        return -r
+
+    def set_data_path(self, path):
+        self.path = path
+        return
+
+    def get_time_data(self):
+        path = self.get_data_path()
+        material = self.get_material()
+
+        data = pd.read_csv(path)
+        data = data.loc[data['material'] == material]
+
+        time = np.append(0, data['time'].values)
+        return time
+
+    def get_adsorption_data(self):
+        path = self.get_data_path()
+        material = self.get_material()
+
+        data = pd.read_csv(path)
+        data = data.loc[data['material'] == material]
+
+        mass_by_mass = np.append(0, data['mass'].values)
+        return mass_by_mass
+
+    def solve_reaction(self, t, k1, k2):
+        c_star = odeint(self.rate_equation, t=t, y0=0, args=(k1, k2))
+        return c_star.flatten()
+
+    def get_reaction_constants(self):
+        t_data = self.get_time_data()
+        c_star_data = self.get_adsorbed_conc()
+
+        popt, pcov = curve_fit(self.solve_reaction, t_data, c_star_data)
+
+        k1, k2 = popt
+        K = k1 / k2
+        return k1, k2, K
--- code/cpm/indoor_sources.py
+++ code/cpm/indoor_sources.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from scipy.integrate import odeint
 from scipy.interpolate import interp1d
+from estimate_kinetics import Kinetics
 
 transient = pd.read_csv('../../data/transient_results.csv', header=4)
 ss = pd.read_csv('../../data/steady_state_material_sweep.csv', header=4)
@@ -26,73 +27,99 @@ transient = pd.concat([transient, p['p_cpm']], axis=1)
 transient['c_ads'] *= 131.38
 
 
-def r(c_in, c_star, K, k2):
-    k1 = K*k2
-    r = k1*c_star-k2*c_in
+def reaction(c_in, c_star, k1, k2, K):
+    k1 = K * k2
+    r = k1 * c_star - k2 * c_in
     return r
 
-def cstr(u, t, Ae, V, V_mat, K, k2):
+
+def cstr(u, t, Ae, V, V_mat, k1, k2, K):
     c_in = u[0]
     c_star = u[1]
-    dc_in_dt = n(t)/V - Ae*c_in + r(c_in, c_star, K, k2)/V
-    dc_star_dt = -r(c_in, c_star, K, k2)/V_mat
+    r = reaction(c_in, c_star, k1, k2, K)
+    dc_in_dt = n(t) / V - Ae * c_in + r / V
+    dc_star_dt = -r / V_mat
 
     return [dc_in_dt, dc_star_dt]
+
 
 # parameters
 Ae = 0.5
 V = 300
-K = 1e-2
-k2 = 1e-4
-M = 131.38 # g/mol TCE
-A_ck = 4*10*1e-2
+M = 131.38  # g/mol TCE
+A_ck = 4 * 10 * 1e-2
 
-n = interp1d(transient['t'].values, transient['j_ck'].values*A_ck*3600*1e2, fill_value=0, bounds_error=False)
-c0_in = n(0)/V/Ae
-transient['c_in'] /= transient['c_in'][0]/c0_in
-
+n = interp1d(transient['t'].values, transient['j_ck'].values *
+             A_ck * 3600 * 1e2, fill_value=0, bounds_error=False)
+c0_in = n(0) / V / Ae
+transient['c_in'] /= transient['c_in'][0] / c0_in
 
 
 # house properties
-A_floor = 10*10 # 10x10 m surface
-A_wall = 10*3 # 10 m wide, 3 m tall
-A_room = A_floor*2 + A_wall*4
-V_room = A_floor*3
+A_floor = 10 * 10  # 10x10 m surface
+A_wall = 10 * 3  # 10 m wide, 3 m tall
+A_room = A_floor * 2 + A_wall * 4
+V_room = A_floor * 3
 # ['Concrete', 'Drywall', 'Wood', 'Wool Carpet', 'Wall Paper']
-penetration_depth = np.array([5e-3, 1e-2, 1e-3, 1e-2, 1e-4])
 
-# order: K, V_mat_i
-config = {
-    'Concrete': [1.977344e-07, A_room*penetration_depth[0]],
-    'Drywall': [3.005379e-03, A_room*penetration_depth[1]],
-    'Wood': [6.626635e-05, A_room*penetration_depth[3]],
-}
+materials = ['concrete', 'wood', 'drywall', 'carpet', 'paper']
+penetration_depths = np.array([5e-3, 1e-3, 1e-2, 1e-2, 1e-4])
+Vs = A_room * penetration_depths
+
+rhos, k1s, k2s, Ks = [], [], [], []
+for material in materials:
+    kin = Kinetics(material=material)
+    rho = kin.get_material_density()
+    k1, k2, K = kin.get_reaction_constants()
+
+    rhos.append(rho)
+    k1s.append(k1)
+    k2s.append(k2)
+    Ks.append(K)
+
+config = pd.DataFrame({'material': materials, 'volume': Vs,
+                       'rho': rhos, 'k1': k1s, 'k2': k2s, 'K': Ks})
+
+
+fig, (ax1, ax2, ax3) = plt.subplots(3, 1, dpi=300, sharex=True)
+ax4 = ax3.twinx()
 
 t = np.linspace(0, 72, 100)
 
-# Concrete, Drywall, Wood
-for key, value in config.items():
-    K = value[0]
-    V_mat = value[1]
-    fig, (ax1, ax2) = plt.subplots(2,1,dpi=300, sharex=True)
-    ax3 = ax2.twinx()
-    for k2 in (1e-4, 1e-2, 1, 1e2, 1e4):
-        k1 = K*k2
-        c0 = [c0_in, c0_in/K]
-        c = odeint(func=cstr, y0=c0, t=t, args=(Ae, V, V_mat, K, k2,))
-        c_in = c[:,0]
-        c_star = c[:,1]
+for index, row in config.iterrows():
+    material = row['material']
+    V_mat = row['volume']
+    k1 = row['k1']
+    k2 = row['k2']
+    K = row['K']
 
-        ax1.semilogy(t,c_in*M,label='k1 = %1.0e, k2 = %1.0e' % (k1, k2))
-        ax2.plot(t,c_star*V_mat*M,label='m_star')
+    c0 = [c0_in, c0_in / K]
+    result = odeint(func=cstr, y0=c0, t=t, args=(Ae, V, V_mat, k1, k2, K))
+    c_in = result[:, 0]
+    c_star = result[:, 1]
 
-        ax3.semilogy(t,c_in*V*M, linestyle='--')
+    ax2.semilogy(t, c_in, label='%s' % material)
+    ax3.semilogy(t, c_star * M * V_mat)
+    ax4.semilogy(t, c_in * M * V, linestyle='--')
 
-    transient.plot(x='t', y='c_in', ax=ax1, logy=True, legend=False, color='k', label='reference')
-    ax1.set_title('%s, K = %1.0e, V_mat = %1.2f m^3' % (key, K, V_mat))
-    ax1.set_ylabel('c_in (g/m^3)')
-    ax2.set_ylabel('m_star (g)')
-    ax3.set_ylabel('m_in (g)')
-    ax2.set_xlabel('time (h)')
-    ax1.legend()
+transient.plot(x='t', y='c_in', ax=ax2, logy=True,
+               legend=False, color='k', label='reference')
+
+table = config.set_index('material')
+table = table.applymap("{0:1.2e}".format)
+
+ax1.axis('off')
+ax1.table(cellText=table.values, colWidths=[0.15] * len(table.columns),
+          rowLabels=table.index,
+          colLabels=table.columns,
+          # cellLoc = 'center', rowLoc = 'center',
+          loc='center')
+
+ax2.set_ylabel('c_in (g/m^3)')
+ax3.set_ylabel('m_star (g)')
+ax4.set_ylabel('m_in (g)')
+ax3.set_xlabel('time (h)')
+ax2.legend()
+
+plt.tight_layout()
 plt.show()
