3224892a3cb902c34805b2f52a2c622330bec5a2
likgoek <jonathanstroem91@gmail.com>
2019-08-21 16:40:50 -0400

    New data and got analysis to work

--- code/adsorption/analysis.py
+++ code/adsorption/analysis.py
@@ -5,14 +5,18 @@ from scipy.integrate import odeint, ode, solve_ivp
 from scipy.optimize import curve_fit, leastsq
 from scipy.interpolate import interp1d
 
+
 class Data:
     def __init__(self, file):
         self.path = file
         return
+
     def get_path(self):
         return self.path
+
     def get_data(self):
         return self.data
+
 
 class Contaminant:
     def __init__(self, contaminant):
@@ -34,6 +38,7 @@ class Contaminant:
     def get_molar_mass(self):
         return self.M
 
+
 class Material:
     def __init__(self, material):
         self.material = material
@@ -42,6 +47,7 @@ class Material:
 
     def get_material(self):
         return self.material
+
     def set_material_density(self):
         material = self.get_material()
         # densities in g/m^3
@@ -57,14 +63,16 @@ class Material:
     def get_materials(self):
         return ['drywall', 'concrete', 'carpet', 'wood', 'paper', 'soil']
 
+
 class COMSOL(Data):
     """
     Class to load, process, and return the COMSOL simulation data
     """
+
     def __init__(self, file):
-        Data.__init__(self,file)
+        Data.__init__(self, file)
         self.process_raw_data()
-        self.A_ck = 0.1*4
+        self.A_ck = 0.1 * 4
         return
 
     def get_crack_area(self):
@@ -75,8 +83,9 @@ class COMSOL(Data):
         return pd.read_csv(path, header=4)
 
     def get_renaming_scheme(self):
-        renaming = {'% Time (h)': 'time','p_in (Pa)': 'p','alpha (1)': 'alpha',
-        'c_in (ug/m^3)': 'c_in','j_ck (ug/(m^2*s))': 'j_ck','m_ads (g)': 'm_ads','c_ads (mol/kg)': 'c_ads'}
+        renaming = {'% Time (h)': 'time', 'p_in (Pa)': 'p', 'alpha (1)': 'alpha',
+                    'c_in (ug/m^3)': 'c_in', 'j_ck (ug/(m^2*s))': 'j_ck', 'm_ads (g)': 'm_ads', 'c_ads (mol/kg)': 'c_ads',
+                    'c_ads/c (1)': 'c_ads/c_soil', 'alpha_ck (1)': 'alpha_ck', 'n_ck (ug/s)': 'n_ck', 'c_soil (ug/m^3)': 'c_soil', 'u_ck (cm/h)': 'u_ck'}
         return renaming
 
     def process_raw_data(self):
@@ -92,13 +101,18 @@ class COMSOL(Data):
         df = self.get_data()
         return df['j_ck'].values
 
+    def get_entry_rate_data(self):
+        df = self.get_data
+        return df['n_ck'].values
+
     def get_concentration_data(self):
         df = self.get_data()
         return df['c_in'].values
 
+
 class Experiment(Data):
-    def __init__(self,file):
-        Data.__init__(self,file)
+    def __init__(self, file):
+        Data.__init__(self, file)
         self.set_raw_data()
         return
 
@@ -106,6 +120,7 @@ class Experiment(Data):
         path = self.get_path()
         self.data = pd.read_csv(path)
         return
+
 
 class Kinetics(Experiment, Material, Contaminant):
     def __init__(self, file, material='concrete', contaminant='TCE', T=298, P=101325):
@@ -125,10 +140,11 @@ class Kinetics(Experiment, Material, Contaminant):
             tuple: temperature (K), absolute pressure (Pa)
         """
         return self.T, self.P
+
     def get_gas_const(self):
         return 8.31446261815324
 
-    def get_gas_conc(self, part_by_part = 1.12e-9):
+    def get_gas_conc(self, part_by_part=1.12e-9):
         """
         Method that converts the air concentration from part by part to mol/m^3
 
@@ -144,7 +160,6 @@ class Kinetics(Experiment, Material, Contaminant):
         M = self.get_molar_mass()
         return P * part_by_part / (R * T)
 
-
     def get_adsorbed_conc(self):
         """
         Return:
@@ -154,7 +169,6 @@ class Kinetics(Experiment, Material, Contaminant):
         rho = self.get_material_density()
         M = self.get_molar_mass()
         return mass_by_mass * rho / 1e9 / M
-
 
     def adsorption_kinetics(self, c_star, t, k1, k2):
         c = self.get_gas_conc()
@@ -169,7 +183,7 @@ class Kinetics(Experiment, Material, Contaminant):
         material = self.get_material()
         data = self.get_data()
         data = data.loc[data['material'] == material]
-        return np.append(0, data['time'].values)/60
+        return np.append(0, data['time'].values) / 60
 
     def get_adsorption_data(self):
         material = self.get_material()
@@ -180,7 +194,8 @@ class Kinetics(Experiment, Material, Contaminant):
         return mass_by_mass
 
     def solve_reaction(self, t, k1, k2):
-        c_star = odeint(self.adsorption_kinetics, t=t, y0=0, args=(k1, k2), mxstep=5000)
+        c_star = odeint(self.adsorption_kinetics, t=t,
+                        y0=0, args=(k1, k2), mxstep=5000)
         return c_star.flatten()
 
     def get_reaction_constants(self):
@@ -190,19 +205,19 @@ class Kinetics(Experiment, Material, Contaminant):
         arg:
             None
         return:
-            k1 : desorption rate (mol/hr)
-            k2 : adsorption rate (mol/hr)
+            k1 : desorption rate (1/hr)
+            k2 : adsorption rate (1/hr)
             K : equilibrium constant (1)
         """
         t_data = self.get_time_data()
         c_star_data = self.get_adsorbed_conc()
 
-        popt, pcov = curve_fit(self.solve_reaction, t_data, c_star_data, p0=[1e-2, 1e2])
+        popt, pcov = curve_fit(self.solve_reaction, t_data,
+                               c_star_data, p0=[1e-2, 1e2])
 
         k1, k2 = popt
         K = k1 / k2
         return k1, k2, K
-
 
     def get_isotherm(self):
         """
@@ -215,11 +230,10 @@ class Kinetics(Experiment, Material, Contaminant):
         """
         k1, k2, K = self.get_reaction_constants()
         rho = self.get_material_density()
-        rho *= 1e-3 # converts to kg/m^3
-        return 1/(K*rho)
+        rho *= 1e-3  # converts to kg/m^3
+        return 1 / (K * rho)
 
-
-    def plot(self,save=False):
+    def plot(self, save=False):
 
         t_data = self.get_time_data()
         c_star_data = self.get_adsorbed_conc()
@@ -228,14 +242,13 @@ class Kinetics(Experiment, Material, Contaminant):
         M = self.get_molar_mass()
         rho = self.get_material_density()
 
-
         t = np.linspace(t_data[0], t_data[-1], 200)
         c_star = self.solve_reaction(t, k1, k2)
 
         fig, ax = plt.subplots(dpi=300)
 
-        ax.plot(t_data*60, c_star_data/rho*M*1e9, 'o', label='Data')
-        ax.plot(t*60, c_star/rho*M*1e9, label='Fit')
+        ax.plot(t_data * 60, c_star_data / rho * M * 1e9, 'o', label='Data')
+        ax.plot(t * 60, c_star / rho * M * 1e9, label='Fit')
 
         ax.set_title('$k_1$ = %1.2e, $k_2$ = %1.2e, $K$ = %1.2e' % (k1, k2, K))
         plt.legend()
@@ -251,18 +264,20 @@ class IndoorSource(COMSOL, Material, Contaminant):
         self.set_building_param()
         self.set_entry_rate()
 
-        if material != None:
+        if material is None:
+            self.material = None
+        else:
             Material.__init__(self, material)
             self.set_material_volume()
-        else:
-            self.material = None
 
         return
 
     def get_air_exchange_rate(self):
         return self.Ae
+
     def get_building_volume(self):
         return self.V
+
     def get_room_area(self):
         return self.A_room
 
@@ -284,15 +299,19 @@ class IndoorSource(COMSOL, Material, Contaminant):
         n = self.get_entry_rate()
         Ae = self.get_air_exchange_rate()
         V = self.get_building_volume()
-        return n(0)/(Ae*V)
+        return n(0) / (Ae * V)
 
     def set_entry_rate(self):
         # gets time and entry rate data
         t = self.get_time_data()
-        j_ck = self.get_entry_flux_data()
-        M = self.get_molar_mass()
-        A_ck = self.get_crack_area()
-        n_ck = j_ck*1e-6/M*3600*A_ck
+
+        try:
+            n_ck = self.get_entry_rate_data() * 1e-6 / M
+        except:
+            j_ck = self.get_entry_flux_data()
+            M = self.get_molar_mass()
+            A_ck = self.get_crack_area()
+            n_ck = j_ck * 1e-6 / M * 3600 * A_ck
         # interpolation function
         func = interp1d(t, n_ck, bounds_error=False, fill_value=n_ck[-1])
         self.n_ck = func
@@ -321,7 +340,6 @@ class IndoorSource(COMSOL, Material, Contaminant):
     def reaction(self, c_in, c_star, k1, k2):
         return k1 * c_star - k2 * c_in
 
-
     def set_reaction_constants(self, k1, k2, K):
         self.k1 = k1
         self.k2 = k2
@@ -348,12 +366,12 @@ class IndoorSource(COMSOL, Material, Contaminant):
         n = self.get_entry_rate()
 
         # odes
-        dc_in_dt = n(t) / V - Ae * c_in + r / V
-        dc_star_dt = -r / V_mat
+        dc_in_dt = n(t) / V - Ae * c_in + r * V_mat / V
+        dc_star_dt = -r
 
         return [dc_in_dt, dc_star_dt]
 
-    def cstr_no_indoor_source(self, t, u):
+    def cstr_no_rxn(self, t, u):
 
         # gets parameters
         Ae = self.get_air_exchange_rate()
@@ -369,66 +387,36 @@ class IndoorSource(COMSOL, Material, Contaminant):
         dc_in_dt = n(t) / V - Ae * c_in
         return dc_in_dt
 
-    def solve_cstr2(self):
+    def solve_cstr(self):
         t = self.get_time_data()
         c0_in = self.get_initial_concentration()
-        k1, k2, K = self.get_reaction_constants()
-        c0 = [c0_in, c0_in / K]
-        t_tmp = t
-        r = solve_ivp(self.cstr, t_span=(t[0], t[-1]), y0=c0, method='Radau', t_eval=t, max_step=0.1, first_step=0.001, nfev=10000, nlu=10000)
 
-        t, c, c_star = r['t'], r['y'][0], r['y'][1]
-        return t, c, c_star
+        if self.get_material() is None:
+            r = solve_ivp(self.cstr_no_rxn, t_span=(
+                t[0], t[-1]), y0=[c0_in], method='Radau', t_eval=t, max_step=0.1, first_step=0.001, nfev=10000, nlu=10000)
+            t, c, c_star = r['t'], r['y'][0], np.repeat(0, len(r['y'][0]))
 
-    def solve_cstr(self):
-        c0_in = self.get_initial_concentration()
-
-        t_data = self.get_time_data()
-        t0, t_end = t_data[0], t_data[-1]
-        t = []
-        c = []
-
-
-        if self.get_material() != None:
+            return t, c, c_star
+        else:
             k1, k2, K = self.get_reaction_constants()
             c0 = [c0_in, c0_in / K]
-            c_star = []
-            r = ode(self.cstr).set_integrator('lsoda', method='bdf', nsteps=5000, max_step=0.2)
-            r.set_initial_value(c0, t0)
-            dt = 1
-            while r.successful() and r.t < t_end:
-                t.append(r.t)
-                c.append(r.y[0])
-                c_star.append(r.y[1])
-
-                r.integrate(r.t+dt)
-            return np.array(t), np.array(c), np.array(c_star)
-        else:
-            c0 = c0_in
-            r = ode(self.cstr_no_indoor_source).set_integrator('lsoda', method='bdf', nsteps=5000, max_step=0.2)
-            r.set_initial_value(c0, t0)
-            dt = 1
-            while r.successful() and r.t < t_end:
-                t.append(r.t)
-                c.append(r.y)
-
-                r.integrate(r.t+dt)
-            return np.array(t), np.array(c)
-
+            r = solve_ivp(self.cstr, t_span=(t[0], t[-1]), y0=c0, method='Radau',
+                          t_eval=t, max_step=0.1, first_step=0.001, nfev=10000, nlu=10000)
+            t, c, c_star = r['t'], r['y'][0], r['y'][1]
+            return t, c, c_star
 
     def get_dataframe(self):
         df = self.get_data()
         t, c, c_star = self.solve_cstr2()
         M = self.get_molar_mass()
-
-        df['c'] = c*M*1e6
-        df['c_star'] = c_star*M*1e6
+        df['c'] = c * M * 1e6
+        df['c_star'] = c_star * M * 1e6
         return df
 
 
 class Analysis(IndoorSource):
 
-    def __init__(self,type):
+    def __init__(self, type):
 
         IndoorSource.__init__()
         return
--- code/adsorption/figures.py
+++ code/adsorption/figures.py
@@ -5,34 +5,23 @@ from analysis import IndoorSource, Kinetics, Material
 
 
 # indoor adsorption figure
-"""
+
 materials = Material('concrete').get_materials()[0:-1] # list of all materials except soil
 
-
+fig, ax = plt.subplots(dpi=300)
 for material in materials:
-    indoor = IndoorSource('../../data/transient_results_shuai_isotherm_good_mesh.csv', material=material)
+    indoor = IndoorSource('../../data/no_soil_adsorption.csv', material=material)
     rxn = Kinetics('../../data/adsorption_kinetics.csv', material=material)
     k1, k2, K = rxn.get_reaction_constants()
     indoor.set_reaction_constants(k1, k2, K)
-    t, c, c_star = indoor.solve_cstr()
+    df = indoor.get_dataframe()
 
-    ax.plot(t, c, label=material)
+    df.plot(x='time', y='c', ax=ax, logy=True, label=material)
+
+
+ref = IndoorSource('../../data/no_soil_adsorption.csv', material=None)
+df_ref = ref.get_dataframe()
+df_ref.plot(x='time', y='c', ax=ax, logy=True, label='Ref')
 
 ax.legend()
-plt.show()
-"""
-material = 'concrete'
-rxn = Kinetics('../../data/adsorption_kinetics.csv', material=material)
-k1, k2, K = rxn.get_reaction_constants()
-indoor = IndoorSource('../../data/transient_results_shuai_isotherm_good_mesh.csv', material=material)
-indoor.set_reaction_constants(k1, k2, K)
-
-
-print(indoor.get_dataframe())
-#t, c, c_star = indoor.solve_cstr2()
-#fig, ax = plt.subplots(dpi=300)
-
-
-ax.plot(t, c)
-
 plt.show()
